a30f2b8a23c5544d892e840938ba646539b9881a
Vitek1625 <witold.kubisiak@gmail.com>
2026-06-12 22:34:43 +0200

    Initial SpringBoot preparation

---
+++ Backend/src/test/java/com/vitek/improWE/ImproWeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitek.improWE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImproWeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
